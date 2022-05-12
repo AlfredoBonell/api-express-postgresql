5b981cbd696ac10fbe1c73be5cde756f316c26ad
Alfredo Bonell <99234057+AlfredoBonell@users.noreply.github.com>
2022-05-11 22:00:14 -0600

    API CRUD - Cliente Servidor.
    - Se agrega la tabla missionCommander y sus endpoints.

--- server.js
+++ server.js
@@ -16,25 +16,12 @@ app.get("/explorers", async (req, res) => {
   res.json(allExplorers);
 });
 
-app.get("/Mission", async (req, res) => {
-  const allMission = await prisma.Mission.findMany({});
-  res.json(allMission);
-});
-
 app.get("/explorers/:id", async (req, res) => {
   const id = req.params.id;
   const explorer = await prisma.explorer.findUnique({
     where: { id: parseInt(id) },
   });
   res.json(explorer);
-});
-
-app.get("/Mission/:id", async (req, res) => {
-  const id = req.params.id;
-  const mission = await prisma.Mission.findUnique({
-    where: { id: parseInt(id) },
-  });
-  res.json(mission);
 });
 
 app.post("/explorers", async (req, res) => {
@@ -45,19 +32,6 @@ app.post("/explorers", async (req, res) => {
   };
   const message = "Explorer creado.";
   await prisma.explorer.create({ data: explorer });
-  return res.json({ message });
-});
-
-app.post("/Mission", async (req, res) => {
-  const mission = {
-    name: req.body.name,
-    lang: req.body.lang,
-    missionCommander: req.body.missionCommander,
-    enrollments: req.body.enrollments,
-    hasCertification: req.body.hasCertification,
-  };
-  const message = "Mission creado.";
-  await prisma.Mission.create({ data: mission });
   return res.json({ message });
 });
 
@@ -76,6 +50,38 @@ app.put("/explorers/:id", async (req, res) => {
   return res.json({ message: "Actualizado correctamente" });
 });
 
+app.delete("/explorers/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.explorer.delete({ where: { id: id } });
+  return res.json({ message: "Eliminado correctamente" });
+});
+
+app.get("/Mission", async (req, res) => {
+  const allMission = await prisma.Mission.findMany({});
+  res.json(allMission);
+});
+
+app.get("/Mission/:id", async (req, res) => {
+  const id = req.params.id;
+  const mission = await prisma.Mission.findUnique({
+    where: { id: parseInt(id) },
+  });
+  res.json(mission);
+});
+
+app.post("/Mission", async (req, res) => {
+  const mission = {
+    name: req.body.name,
+    lang: req.body.lang,
+    missionCommander: req.body.missionCommander,
+    enrollments: req.body.enrollments,
+    hasCertification: req.body.hasCertification,
+  };
+  const message = "Mission creado.";
+  await prisma.Mission.create({ data: mission });
+  return res.json({ message });
+});
+
 app.put("/Mission/:id", async (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -91,15 +97,56 @@ app.put("/Mission/:id", async (req, res) => {
   return res.json({ message: "Actualizado correctamente" });
 });
 
-app.delete("/explorers/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  await prisma.explorer.delete({ where: { id: id } });
-  return res.json({ message: "Eliminado correctamente" });
-});
-
 app.delete("/Mission/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   await prisma.Mission.delete({ where: { id: id } });
+  return res.json({ message: "Eliminado correctamente" });
+});
+
+app.get("/missionCommander", async (req, res) => {
+  const allMissionComander = await prisma.missionCommander.findMany({});
+  res.json(allMissionComander);
+});
+
+app.get("/missionCommander/:id", async (req, res) => {
+  const id = req.params.id;
+  const missionComander = await prisma.missionCommander.findUnique({
+    where: { id: parseInt(id) },
+  });
+  res.json(missionComander);
+});
+
+app.post("/missionCommander", async (req, res) => {
+  const missionCommander = {
+    name: req.body.name,
+    username: req.body.username,
+    mainStack: req.body.mainStack,
+    currentEnrollment: req.body.currentEnrollment,
+    hasAzureCertification: req.body.hasAzureCertification,
+  };
+  const message = "Mission Comander creado.";
+  await prisma.missionCommander.create({ data: missionCommander });
+  return res.json({ message });
+});
+
+app.put("/missionCommander/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  await prisma.missionCommander.update({
+    where: {
+      id: id,
+    },
+    data: {
+      mainStack: req.body.mainStack,
+    },
+  });
+
+  return res.json({ message: "Actualizado correctamente" });
+});
+
+app.delete("/missionCommander/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.missionCommander.delete({ where: { id: id } });
   return res.json({ message: "Eliminado correctamente" });
 });
 
